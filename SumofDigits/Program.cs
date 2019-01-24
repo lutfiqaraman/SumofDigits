0cf0c6976566ad8877133a2f75801bbc36a611a0
lutfiqaraman <lutfiqaraman@gmail.com>
2019-01-24 23:16:30 +0200

    Done

--- SumofDigits/Program.cs
+++ SumofDigits/Program.cs
@@ -10,6 +10,23 @@ namespace SumofDigits
     {
         static void Main(string[] args)
         {
+            string input = Console.ReadLine();
+            int a1 = Convert.ToInt32(input);
+
+            int sum = 0;
+            string a2 = Convert.ToString(a1);
+
+            Console.WriteLine("The digits");
+
+            foreach (var item in a2)
+            {
+                
+                Console.WriteLine(item);
+                sum += Convert.ToInt32(item.ToString());
+            }
+
+            Console.WriteLine("The sum of digits {0}", sum);
+            Console.ReadLine();
         }
     }
 }
